3c732035a49512db5ba1c4f14683d2778dab5e69
Aiken Tine Ahac <ahac.aiken@gmail.com>
2021-02-27 20:24:07 +0100

    🚑 Update README and critical meme fix

--- commands.js
+++ commands.js
@@ -1,58 +1,54 @@
 const https = require('https');
-const { MessageEmbed } = require('discord.js');
+const Discord = require('discord.js');
 
 module.exports = {
-    memeGen: function(message, url) {
+    memeGen: function (message, url) {
 
         https.get(url, (result) => {
-            let body = '';
-
-            result.on('data', (chunk) => body += chunk)
+            var body = ''
+            result.on('data', (chunk) => {
+                body += chunk
+            })
 
             result.on('end', () => {
-                const response = JSON.parse(body);
-                const index =response.data.children[Math.floor(Math.random() * 99)+1].data;
-
-                if (index.post_hint !== image) {
-                    
-                    const text = index.selftext;
-                    const textembed = new MessageEmbed()
-                        .setTitle(subRedditName)
-                        .setColor(9384170)
-                        .setDescription(`[${title}](${link})\n\n${text}`)
-                        .setURL(`https://reddit.com/${subRedditName}`)
-                
-                    console.log("Sending meme");
-                    message.channel.send(textembed);
-                }
-
-                let image = index.preview.images[0].source.url('&amp;', '&');
-                let title = index.title;
-                let link = 'https://reddit.com' + index.permalink;
-                let subRedditName = index.subreddit_name_prefixed
+                var response = JSON.parse(body)
+                var index = response.data.children[Math.floor(Math.random() * 99) + 1].data
 
                 if (index.post_hint !== 'image') {
-                    const textembed = new MessageEmbed()
+
+                    var text = index.selftext
+                    const textembed = new Discord.MessageEmbed()
                         .setTitle(subRedditName)
                         .setColor(9384170)
                         .setDescription(`[${title}](${link})\n\n${text}`)
                         .setURL(`https://reddit.com/${subRedditName}`)
 
-                    console.log("Sending meme");
+                    message.channel.send(textembed)
+                }
+
+                var image = index.preview.images[0].source.url.replace('&amp;', '&')
+                var title = index.title
+                var link = 'https://reddit.com' + index.permalink
+                var subRedditName = index.subreddit_name_prefixed
+
+                if (index.post_hint !== 'image') {
+                    const textembed = new Discord.RichEmbed()
+                        .setTitle(subRedditName)
+                        .setColor(9384170)
+                        .setDescription(`[${title}](${link})\n\n${text}`)
+                        .setURL(`https://reddit.com/${subRedditName}`)
+
                     message.channel.send(textembed)
                 }
                 console.log(image);
-
-                const imageembed = new MessageEmbed()
+                const imageembed = new Discord.MessageEmbed()
                     .setTitle(subRedditName)
                     .setImage(image)
                     .setColor(9384170)
                     .setDescription(`[${title}](${link})`)
                     .setURL(`https://reddit.com/${subRedditName}`)
-                
-                console.log("Sending meme");
                 message.channel.send(imageembed)
-            }).on('error', function(e) {
+            }).on('error', function (e) {
                 console.log('Got an error: ', e)
             })
         })
